a11b3420fd5aa440480480caa2ed23e7de7e66f6
Matheus Andrade <matheusas301@gmail.com>
2023-10-31 20:01:23 -0300

    Criação do header e sua estilização

---
+++ src/app/components/Header/Header.style.ts
@@ -0,0 +1,97 @@
+import styled from "styled-components"
+import Lupa from "../../assets/LupaDePesquisa.svg"
+
+export const StyledHeader = styled.header`
+    background-color: #E2E2FF;
+    width: 100%;
+    padding-top: 3vh;
+
+@media (min-width: 320px) {
+    height: 6vh;
+    padding-top: 1.5vh;
+}
+
+@media (min-width: 768px) {
+    height: 10vh;
+    padding-top: 3vh;
+}
+
+
+@media (min-width: 1024px) {
+    height: 10vh;
+}    
+`
+
+export const StyledDivHeader = styled.div`
+    display: flex; 
+    align-items: center; 
+    justify-content: space-between;
+    margin-left: 32px;   
+
+    @media (min-width: 320px) {
+        margin-left: 10px;
+        margin-top: 0;
+    }
+
+    @media (min-width: 768px) {
+        margin-left: 14px;
+    }
+
+    @media (min-width: 1024px) {
+        margin-left: 17px;
+    }
+
+` 
+
+export const StyledInputHeader = styled.input`
+    width: 700px;
+    height: 40px;
+    border-radius: 150px;
+    background-image: url(${Lupa});
+    background-position: 2%;
+    background-repeat: no-repeat;
+    border: 1px solid white;
+    text-align: center;
+    margin-right: 205px;
+    
+${(props) =>
+props.RemoverInput &&`
+    display: none;
+`
+}
+
+@media (min-width: 320px) {
+    display: none;
+}
+
+@media (min-width: 768px) {
+    display: block;
+    width: 350px;
+    height: 40px;
+    margin-right: 70px;
+}
+
+@media (min-width: 1024px) {
+    font-size: 18px;
+    width: 450px;
+    margin-left: 40px;
+}
+`
+
+export const StyledImgPorto = styled.img`
+    width: 100px;
+    margin-left: 3vw;
+
+@media (min-width: 320px) {
+    width: 90px;
+    margin-left: 0;
+}
+
+@media (min-width: 768px) {
+    width: 130px;
+}
+
+@media (min-width: 1024px) {
+    width: 150px;
+}
+`
---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LogoPorto from '../../assets/porto-icone-header.svg'
+import Dropdown from '../../components/MenuNav/DropDown'
+import { StyledHeader } from './Header.style'
+import { StyledDivHeader } from './Header.style'
+import { StyledInputHeader } from './Header.style'
+import { StyledImgPorto } from './Header.style'
+
+interface InputProps{
+    RemoverInput?: boolean
+}
+
+export const Header = ({RemoverInput}: InputProps) => {
+
+    return (
+        <header>
+            <StyledHeader>
+            <StyledDivHeader>
+                <StyledImgPorto src={LogoPorto} alt="Logo da Porto Seguro" />
+                <StyledInputHeader type="search" name="input-header" id="input-header" placeholder='Digite aqui o que procura' RemoverInput = {RemoverInput}/>
+               <Router>
+                    <Dropdown />
+                </Router>
+            </StyledDivHeader>
+            </StyledHeader>
+        </header>
+        
+    )
+}
