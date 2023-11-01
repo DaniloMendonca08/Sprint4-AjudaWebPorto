4f1e920087c8404e2efb1aad4aa396bd2e5130b9
Arthur Lima <arthurwrx@outlook.com>
2023-11-01 15:05:00 -0300

    pagina de acionamento de sinistro

---
+++ src/app/AcionamentoSinistro/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import { BodyAcionamento } from "../components/BodyAcionamento/BodyAcionamento";
+
+export const AcionamentoSinistro = () => {
+    return(
+        <>
+        <Header RemoverInput/>
+        <BodyAcionamento />
+        <Footer />
+        </>
+    )
+}
