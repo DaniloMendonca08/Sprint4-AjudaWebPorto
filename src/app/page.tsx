dd3ddfd534254ac98bc86694f88fe2e87f7b1a0b
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-10 16:44:55 -0300

    testes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import Dropdown from './components/MenuNav/DropDown'
 import { SolucaoPorto } from './SolucaoPorto/page'
 import GlobalStyles from '@/styles/globalStyles'
+import { CentralSolucoes } from './CentralSolucoes/page'
+import { FazerCadastro } from './FazerCadastro/page'
 
 export default function Home() {
   return (
     <>
     <GlobalStyles />
-    <SolucaoPorto />
+    <FazerCadastro />
     </>
   )
 }
