22fc691baab10ec548aa758f17391c1c13a79e0f
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-10 16:43:33 -0300

    page-cadastro

---
+++ src/app/FazerCadastro/page.tsx
@@ -0,0 +1,13 @@
+import { BodyCadastro } from "../components/BodyCadastro/BodyCadastro"
+import { Footer } from "../components/Footer/Footer"
+import { Header } from "../components/Header/Header"
+
+export const FazerCadastro = () => {
+    return(
+        <>
+        <Header />
+        <BodyCadastro />
+        <Footer />
+        </>
+    )
+}
