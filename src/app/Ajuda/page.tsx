e74d7ea903c2fe93b8f0fe0517f30858c6ff156a
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-10 16:45:38 -0300

    ajustes

--- src/app/Ajuda/page.tsx
+++ src/app/Ajuda/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { BodySolucoes } from "../components/BodySolucoes/BodySolucoes";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
@@ -5,7 +7,7 @@ import imgAjuda from "../../assets/ImagemAjuda.svg"
 
 export const paragrafoAjuda = "Nesta área, pensamos em ser uma parte mais abrangente para qualquer problema que o assegurado queira reportar, gerenciado por uma inteligência, ela aconselhará o assegurado e o guiará para fazer com que ele faça a melhor escolha. Por esta área ser mais abrangente, ela estará preparada para muitas possibilidades que o assegurado traga para resolver, fazendo com que seja aconselhado a melhor escolha entre todas as possiveis."
 
-export const Ajuda = () => {
+const Ajuda = () => {
     return ( <>
         <Header RemoverInput/>
         <BodySolucoes titulo = {"Ajuda"} img = {imgAjuda} texto ={paragrafoAjuda} descricao="Imagem ilustrando uma ajuda"/>
@@ -13,3 +15,5 @@ export const Ajuda = () => {
         </>)
    
 }
+
+export default Ajuda
