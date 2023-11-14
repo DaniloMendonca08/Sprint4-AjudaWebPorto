a12162e764f97ab92da6f871375165c5c47bdadc
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-13 22:33:18 -0300

    arquivo-colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    primary: {
+      purple: "#E2E2FF",
+      blue: "#00A1FC",
+      dark: "#000000",
+      light: "#FFFFFF",
+    },
+    form: {
+      borderInput: "#c3c3ff",
+      backgroundInput: "#F8F8F8",
+      label : "#004ea1",
+      borderButton: "#00ABFF",
+    }
+
+  }
