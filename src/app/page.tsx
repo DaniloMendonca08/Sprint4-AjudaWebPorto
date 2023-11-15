5bea5682c6bb43248ec68cc6edfa5c87bfd0c4a7
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 20:55:11 -0300

    mais-testes-vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ import SolucaoPorto from './SolucaoPorto/page'
 import GlobalStyles from '@/styles/globalStyles'
 import Layout from './layout'
 import { BodyPagPrincipal } from './components/BodyPagPrincipal/BodyPagPrincipal'
+import { Header } from './components/Header/Header'
+import { Footer } from './components/Footer/Footer'
 
 
 export default function Home() {
   return (
     <>
-    <Layout>
+    <Header />
       <SolucaoPorto />
-    </Layout>
+    <Footer />
     </>
   )
 }
