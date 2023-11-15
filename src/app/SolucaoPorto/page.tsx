fc461a438265b3817a7c51671fe8b9971dc9ba9e
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 19:43:09 -0300

    teste

--- src/app/SolucaoPorto/page.tsx
+++ src/app/SolucaoPorto/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { Header } from "../components/Header/Header";
 import { BodyPagPrincipal } from "../components/BodyPagPrincipal/BodyPagPrincipal";
 import { Footer } from "../components/Footer/Footer";
