a6562ff6d05efce943cd99d88297b853fa20e199
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 12:59:55 -0300

    att-layout-tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 "use client"
 
 import GlobalStyles from '@/styles/globalStyles'
-import { SolucaoPorto } from './SolucaoPorto/page'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
-import { BodyPagPrincipal } from './components/BodyPagPrincipal/BodyPagPrincipal'
-
 
 export default function RootLayout({
   children,
