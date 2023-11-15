0eb8fe77d7a61325e7eab9f70bff5f164825022a
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 20:58:30 -0300

    ajuste-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,7 @@ export default function Layout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="pt-BR">
       <GlobalStyles />
-      <Header />
       <body>{children}</body>
-      <Footer />
     </html>
   )
 }
