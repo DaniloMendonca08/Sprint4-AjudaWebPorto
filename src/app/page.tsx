93631b4dc24b31259181e153dd6f023167292448
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 23:33:29 -0300

    testes-page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,12 @@
 
 import GlobalStyles from '@/styles/globalStyles'
 import SolucaoPorto from './SolucaoPorto/page'
-import BodyPagPrincipal from './components/BodyPagPrincipal/BodyPagPrincipal'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
 
 export default function Home() {
   return (
     <>
     <GlobalStyles />
-    <Header />
-    <BodyPagPrincipal/>
-    <Footer />
+    <SolucaoPorto />
     </>
   )
 }
