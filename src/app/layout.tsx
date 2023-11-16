f27355e0288a6cc261bc301cd17fc71db1e9d07c
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 23:19:10 -0300

    remove-imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 "use client"
 
 import GlobalStyles from '@/styles/globalStyles'
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
 
 export default function Layout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="pt-BR">
       <GlobalStyles />
-      <Header />
       <body>{children}</body>
-      <Footer />
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client"
 
 import SolucaoPorto from './SolucaoPorto/page'
-import GlobalStyles from '@/styles/globalStyles'
-import Layout from './layout'
-import BodyPagPrincipal from './components/BodyPagPrincipal/BodyPagPrincipal'
-import { BodyAcionamento } from './components/BodyAcionamento/BodyAcionamento'
-
 
 export default function Home() {
   return (
