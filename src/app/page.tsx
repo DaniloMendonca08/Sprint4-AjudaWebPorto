ec8798398d9bfbe2fdd116e85f8cf72895a9969f
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 23:29:56 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import SolucaoPorto from './SolucaoPorto/page'
+import BodyPagPrincipal from './components/BodyPagPrincipal/BodyPagPrincipal'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <>
     <Header />
-    <SolucaoPorto />
+    <BodyPagPrincipal/>
     <Footer />
     </>
   )
