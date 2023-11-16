36bd4e176fafd0a7747caf73298a446807794362
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 23:28:00 -0300

    att-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import SolucaoPorto from './SolucaoPorto/page'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header/Header'
 
 export default function Home() {
   return (
     <>
+    <Header />
     <SolucaoPorto />
+    <Footer />
     </>
   )
 }
