ba4b3aaaec46b9f9426ef35d3b3881d96ee41383
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-15 23:02:37 -0300

    testes-vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ export default function Layout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="pt-BR">
       <GlobalStyles />
-      <Header />
       <body>{children}</body>
       <Footer />
     </html>
